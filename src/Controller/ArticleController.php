faf251fe49b5a48fa48313b32f1bdaf15ffe59ab
Piou-piou <pilloud.anthony@gmail.com>
2024-11-20 18:38:45 +0100

    feat: add first form for article

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -3,35 +3,43 @@
 namespace App\Controller;
 
 use App\Entity\Article;
+use App\Form\ArticleType;
 use App\Repository\ArticleRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class ArticleController extends AbstractController
 {
     #[Route('/article/create', name: 'article_create')]
     #[Route('/article/edit/{id}', name: 'article_edit')]
-    public function edit(EntityManagerInterface $em, ?Article $article)
+    public function edit(Request $request, EntityManagerInterface $em, ?Article $article)
     {
         if (!$article) {
             $article = new Article();
-            $article->setTitle('Mon nouvel article');
-            $article->setContent('Lorem ipsum');
-            $em->persist($article);
-            $em->flush();
-        } else {
-            $article->setTitle('Mon nouvel article rename2');
-            $em->persist($article);
-            $em->flush();
         }
 
-        return $this->redirectToRoute('article_show', [
-            'slug' => $article->getSlug(),
-            'id' => $article->getId(),
+        $form = $this->createForm(ArticleType::class, $article);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $article = $form->getData();
+            $em->persist($article);
+            $em->flush();
+
+            $this->addFlash('success', 'Article saved.');
+
+            return $this->redirectToRoute('article_show', [
+                'slug' => $article->getSlug(),
+                'id' => $article->getId()
+            ]);
+        }
+
+        return $this->render('article/edit.html.twig', [
+            'form' => $form
         ]);
     }
 
