4b1769518e513c42240848418483aedbed820688
Piou-piou <pilloud.anthony@gmail.com>
2024-11-20 19:01:05 +0100

    feat: add constraints on form

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 use App\Repository\ArticleRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: ArticleRepository::class)]
 #[ORM\HasLifecycleCallbacks]
@@ -15,9 +16,11 @@ class Article
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\Length(min: 2, max: 255)]
     private ?string $title = null;
 
     #[ORM\Column(type: Types::TEXT, nullable: true)]
+    #[Assert\Length(min: 150, max: 255)]
     private ?string $content = null;
 
     #[ORM\Column(length: 255)]
